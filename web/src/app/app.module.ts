b3caeb4d0a6ab42d3ce7e8bb9236118b4d4d838f
KennyTheBard <octav_teculescu@yahoo.com>
2021-02-01 15:17:50 +0200

    Added interceptors

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { RequestInterceptor } from './interceptors/request.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
   providers: [
-    // TODO: add interceptors for requests and responses
+    RequestInterceptor,
+    ResponseInterceptor,
   ],
   bootstrap: [AppComponent]
 })
--- web/src/app/features/public/public.module.ts
+++ web/src/app/features/public/public.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PublicRoutingModule } from './public-routing.module';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ActivateComponent } from './pages/activate/activate.component';
-import { RecoverComponent } from './pages/recover/recover.component';
-import { ResetComponent } from './pages/reset/reset.component';
 import { publicPages } from './pages';
 
 
